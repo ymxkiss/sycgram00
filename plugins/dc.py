3c0e3e589287b7f4c9ae676f19066f89182a2d79
JiRan <54714341+h88782481@users.noreply.github.com>
2022-09-11 23:19:13 +0800

    Update dc.py

--- plugins/dc.py
+++ plugins/dc.py
@@ -10,6 +10,6 @@ async def dc(_: Client, msg: Message):
     _is_replied = bool(msg.reply_to_message)
     dc_id = msg.reply_to_message.from_user.dc_id \
         if _is_replied else msg.chat.dc_id
-    name = msg.reply_to_message.from_user.mention(style="md") \
+    name = msg.reply_to_message.from_user.mention(style=ParseMode.MARKDOWN) \
         if _is_replied else f"`{msg.chat.title or msg.chat.first_name}`"
     await msg.edit_text(get_dc_text(name, dc_id))
